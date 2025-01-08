f8ae58dce1f83a55dbca23a63847521e6a5accf1
Tom Armitage <tom@infovore.org>
2025-01-08 16:03:10 +0000

    Add mdsvex.

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,4 @@
+import { mdsvex } from "mdsvex";
 import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
@@ -5,7 +6,7 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 const config = {
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
-  preprocess: vitePreprocess(),
+  preprocess: [vitePreprocess(), mdsvex({ extensions: [".svx", ".md"] })],
 
   kit: {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
@@ -17,6 +18,8 @@ const config = {
     },
     appDir: "app",
   },
+
+  extensions: [".svelte", ".svx", ".md"],
 };
 
 export default config;
