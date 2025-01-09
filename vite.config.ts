9c6e163198b1f89417c41a73a6aa3ce5074ab9f4
Tom Armitage <tom@infovore.org>
2025-01-09 13:08:03 +0000

    Store latest firmware per-device.
    This is what powers the instruction top right to upgrade. Because of the
    way things work, we can - and should - do this _per-device_. So:
    
    - each device config contains the latest semver for that device's
      firmware, and the URL to get it
    - that's what gets tested against the number transmitted over sysex.
    
    Neat.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
-import { version, config } from "./package.json";
+import { version } from "./package.json";
 
 export default defineConfig({
   define: {
     __BUILD_VERSION__: `'${version}'`,
-    __FIRMWARE_VERSION__: `'${config.firmwareVersion}'`,
   },
   plugins: [sveltekit()],
 });
