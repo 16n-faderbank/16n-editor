a9984e923f2d05412a2d4763f3e889231f40ad77
Tom Armitage <tom@infovore.org>
2025-01-15 13:47:02 +0000

    Yellow, not red, for a warning, not error.

--- src/lib/components/InvalidHiresController.svelte
+++ src/lib/components/InvalidHiresController.svelte
@@ -6,20 +6,21 @@
 <div>
   <p>
     <strong>Warning: </strong>
-    You have an one or more invalid {type.toLocaleUpperCase()} CC assignments.
+    You have an one or more out-of-spec {type.toLocaleUpperCase()} CC assignments.
   </p>
   <p>
     The MIDI spec only allows for high-resolution CCs for CCs 0-31.
-    <strong>However</strong>, some software is absolutely fine with
+    <strong>However</strong>, some hardware or software is absolutely fine with
     high-resolution CCs on any channel. Check the documentation for your
-    software; your faderbank configuration may still function correctly.
+    hardware or software; your faderbank configuration may still function
+    correctly.
   </p>
 </div>
 
 <style>
   div {
-    border: 2px solid darkred;
-    background: #ff000044;
+    border: 2px solid goldenrod;
+    background: #daa52044;
     padding: 1rem;
     margin: 1rem 0;
   }
